06d7d3495aca29006bc1ee2a836e5472bdebc47f
Dhruv-Prasad <dhruvpras@gmail.com>
2025-04-02 15:29:25 +0530

    working now with 3 buffers

--- a3/server-wfq.py
+++ a3/server-wfq.py
@@ -8,8 +8,9 @@ SERVER_IP = "127.0.0.1"
 SERVER_PORT = 4000
 CAPACITY = 10  # packets per second
 PACKET_SIZE = 1024
-BUFFER_SIZE = 10 # Max queue size
-FLOW_WEIGHTS = {5001: 1, 5002: 1, 5003: 1}  # weights for each flow
+BUFFER_SIZE = 1000 # Max queue size
+FLOW_WEIGHTS = {5001: 8, 5002: 1, 5003: 1}  # weights for each flow
+SIZE_CNT = [0,0,0]
 
 class WFQServer:
     def __init__(self):
@@ -26,29 +27,48 @@ class WFQServer:
         vft = max(arrival_time, last_vft) + 1 / (CAPACITY * weight)
         self.last_vft[flow] = vft
         return vft
-
-    def enqueue_packet(self, packet, flow, arrival_time):
-        vft = self.compute_vft(flow, arrival_time)
+    def find_flow_ind(self,flow):
+        flow_ind = 0
+        for key in FLOW_WEIGHTS.keys():
+            if key == flow:
+                break
+            else:
+                flow_ind+=1
+        return flow_ind
+    
+    def enqueue_packet(self, packet, flow):
         with self.lock:
-            heapq.heappush(self.buffer, (vft, (packet, flow)))
-            if len(self.buffer) >= BUFFER_SIZE:
+            flow_ind = self.find_flow_ind(flow)
+            vft = self.compute_vft(flow, self.virtual_time)
+            if SIZE_CNT[flow_ind] < BUFFER_SIZE:
                 # Drop the packet with the highest VFT
                 # since we are removing the last element
                 # the tree does not change so you don't
                 # need to heapify.
-                self.buffer.pop()
+                heapq.heappush(self.buffer, (vft, (packet, flow)))
+                SIZE_CNT[flow_ind]+=1
+                print(f"received on {flow_ind} at {self.virtual_time}, vft = {vft}", flush=True)
                 # heapify is O(n), we don't want that
                 # heapq.heapify(self.buffer)
+            
 
     def serve_packets(self):
         while True:
-            time.sleep(1 / CAPACITY)  # Serve packets at the given capacity
-            with self.lock:
-                if self.buffer:
-                    vft, (packet, flow) = self.buffer.pop(0)
-                    self.virtual_time = vft
-                    client_addr = ("127.0.0.1", flow)
-                    self.sock.sendto(packet, client_addr)
+            self.lock.acquire()
+            if self.buffer:
+                vft, (packet, flow) = heapq.heappop(self.buffer)
+                self.lock.release()
+                time.sleep(1/CAPACITY)
+                flow_ind = self.find_flow_ind(flow)
+                SIZE_CNT[flow_ind]-=1
+                # print(self.buffer)
+                self.virtual_time = vft
+                client_addr = ("127.0.0.1", flow)
+                self.sock.sendto(packet, client_addr)
+                print(flow_ind, " ", vft)
+            else:
+                self.lock.release()
+            
 
     def start(self):
         # didn't daemon make it like very slow??
@@ -58,9 +78,8 @@ class WFQServer:
         while True:
             data, addr = self.sock.recvfrom(PACKET_SIZE)
             flow_port = addr[1]  # Extract client port number from address tuple
-            arrival_time = self.virtual_time
             if flow_port in FLOW_WEIGHTS:
-                self.enqueue_packet(data, flow_port, arrival_time)
+                self.enqueue_packet(data, flow_port)
 
 if __name__ == "__main__":
     server = WFQServer()
